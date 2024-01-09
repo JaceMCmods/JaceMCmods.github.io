f858747b00962cff42f3808a7847ce35e366754b
JaceMCmods <140463230+JaceMCmods@users.noreply.github.com>
2024-01-09 13:16:26 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function redirectToBanana() {
+  window.location.href = "page3.htm";
+}
+
+function redirectToMonkey() {
+  window.location.href = "page2.htm";
+}
+
+function RedirectToHome() {
+  window.location.href = "page1.htm";
+}
+
+function RedirectToPage4() {
+  window.location.href = "page4.htm";
+}
+
+
